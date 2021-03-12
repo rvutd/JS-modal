1653b7ae6d329a77ae8eb9f3a7cea317e94cdf04
Ravindra Purohit <ravindrapurohit2011@gmail.com>
2021-03-12 20:47:06 +0530

    'all_done'

--- app.js
+++ app.js
@@ -3,4 +3,14 @@
 // when user clicks modal-btn add .open-modal to modal-overlay
 // when user clicks close-btn remove .open-modal from modal-overlay
 
+openModal = document.querySelector('.modal-btn');
+closeModal = document.querySelector('.close-btn');
+overlayModal = document.querySelector('.modal-overlay');
 
+openModal.addEventListener('click', function(){
+    overlayModal.classList.add('open-modal')
+})
+
+closeModal.addEventListener('click',()=>{
+    overlayModal.classList.remove('open-modal');
+})
